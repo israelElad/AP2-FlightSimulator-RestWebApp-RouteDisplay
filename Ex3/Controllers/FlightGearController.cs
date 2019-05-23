3e0256b7e781098a626dc6b26184023ec1dae156
Narkis Kremizi <narkis.kremizi@live.biu.ac.il>
2019-05-23 11:49:55 +0300

    open is good

---
+++ Ex3/Controllers/FlightGearController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ex3.Controllers
+{
+    public class FlightGearController : Controller
+    {
+        // GET: FlightGear
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult display()
+        {
+            return View();
+        }
+    }
+}
